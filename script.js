9b1c6cd67a6157b7051e2dfe19be7c46e8fc2773
edhansa <119650855+edhansa@users.noreply.github.com>
2022-12-06 17:31:51 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,21 +1,2 @@
-async function getUser() {
-    try {
-      const response = await fetch('data.json', {
-        method: 'POST',
-        headers: {
-          accept: 'application/json',
-        },
-      });
-  
-      if (!response.ok) {
-        throw new Error(`Error! status: ${response.status}`);
-      }
-  
-      const result = await response.json();
-      return result;
-    } catch (err) {
-      console.log(err);
-    }
-  }
-  
-  getUser();
+fetch('data.json')
+   .then(res=>res.json())
