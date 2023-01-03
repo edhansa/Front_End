7e1a2bd745d54002f856057d7ba8c41071ac96e7
edhansa <119650855+edhansa@users.noreply.github.com>
2023-01-03 13:25:50 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,35 @@
-fetch("data.json")
-   .then(res=> res.json())
-   .then(data=> console.log(data))
+function afficherContenuJSON() {
+  // Récupération de l'élément div
+  var el = document.getElementById("maDiv");
+
+  // Création d'une requête HTTP
+  var xhr = new XMLHttpRequest();
+
+  // Ouverture de la requête
+  xhr.open("GET", "data.json", true);
+
+  // Envoi de la requête
+  xhr.send();
+
+  // Gestion de la réponse
+  xhr.onload = function() {
+    if (xhr.status === 200) {
+      // Si la réponse est réussie, on affiche le contenu du fichier JSON dans la div
+      el.innerHTML = xhr.responseText;
+    } else {
+      // Si la réponse est un échec, on affiche un message d'erreur
+      el.innerHTML = "Erreur lors de la récupération du fichier JSON";
+    }
+  }
+}
+Pour utiliser cette fonction, il vous suffit de l'appeler lorsque vous souhaitez afficher le contenu du fichier JSON dans la div. Par exemple :
+
+Copy code
+<button onclick="afficherContenuJSON()">Afficher le contenu du fichier JSON</button>
+N'oubliez pas d'inclure l'élément div dans votre code HTML :
+
+Copy code
+<div id="maDiv"></div>
+
+
+
