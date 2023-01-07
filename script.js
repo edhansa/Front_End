1bc0a526757b216fd530bfe5a1c65281deeb3a9b
edhansa <119650855+edhansa@users.noreply.github.com>
2023-01-07 21:04:50 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,41 @@
+function afficherLignes(option) {
+  // Récupération de l'élément div
+  var el = document.getElementById("maDiv");
+
+  // Création d'une requête HTTP
+  var xhr = new XMLHttpRequest();
+
+  // Ouverture de la requête
+  xhr.open("GET", "data.json", true);
+
+  // Envoi de la requête
+  xhr.send();
+
+  // Gestion de la réponse
+  xhr.onload = function() {
+    if (xhr.status === 200) {
+      // Si la réponse est réussie, on convertit le contenu du fichier JSON en objet JavaScript
+      var json = JSON.parse(xhr.responseText);
+
+      
+      var lignes = [];
+
+      
+      for (var i = 0; i < json.length; i++) {
+        if (json[i].Column1 === option) {
+          lignes.push(json[i]);
+        }
+      }
+
+      el.innerHTML = lignes.join("<br>");
+    } else {
+      // Si la réponse est un échec, on affiche un message d'erreur
+      el.innerHTML = "Erreur lors de la récupération du fichier JSON";
+    }
+  }
+}
+
+
 function afficherContenuJSON() {
   // Récupération de l'élément div
   var el = document.getElementById("maDiv");
